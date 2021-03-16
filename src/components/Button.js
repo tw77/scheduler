7e726ff2c6004e9517cb3a29cc8939ec546df1f3
Thomas Weideman <44182629+tw77@users.noreply.github.com>
2021-03-15 17:17:54 -0700

    Add Button and DayListItem

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
-
 import "components/Button.scss";
 
+const classNames = require('classnames');
+
+
 export default function Button(props) {
-   return <></>;
+   let buttonClass = classNames("button", {
+      "button button--confirm": props.confirm,
+      "button button--danger": props.danger
+   });
+
+  return (
+   <button
+     className={buttonClass}
+     onClick={props.onClick}
+     disabled={props.disabled}
+   >
+     {props.children}
+   </button>
+ );
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+const classNames = require('classnames');
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+ });
+
+  function formatSpots(spots) {
+    if (spots > 1) {
+      return (props.spots + " spots remaining");
+    }
+    if (spots === 1) {
+      return (props.spots + " spot remaining");
+    }
+    else return "no spots remaining";
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
