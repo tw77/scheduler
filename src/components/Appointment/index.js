6e69564df9f9b6b1c0b27dd2a8324df6866dd5de
Thomas Weideman <44182629+tw77@users.noreply.github.com>
2021-03-18 16:31:44 -0700

    Render Show, Empty, Create modes conditionally

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React from "react";
 import "components/Appointment/styles.scss";
 
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  console.log("props.interview: ", props.interview);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show name={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty /> }
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          name={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
-  )
+  );
 }
