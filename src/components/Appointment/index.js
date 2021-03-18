2ab31286eddcf5d9dff388eef4645578a15ac0fa
Thomas Weideman <44182629+tw77@users.noreply.github.com>
2021-03-17 17:17:00 -0700

    Remove unused data

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,61 +5,6 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time:  "2pm",
-    interview: {
-      student: "M",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time:  "3pm",
-    interview: {
-      student: "N",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time:  "4pm",
-    interview: {
-      student: "O",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
-
 export default function Appointment(props) {
   return (
     <article className="appointment">
